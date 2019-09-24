8e6c6bde5a194452a79dfe1f78ec0eca4482c853
Joshua Franco <jtf8@njit.edu>
2019-09-24 13:54:42 -0400

    SampleSocketServer.java

--- SampleSocketClient.java
+++ SampleSocketClient.java
@@ -70,7 +70,7 @@ public class SampleSocketClient {
 	}
 	public static void main(String[] args) {
 		SampleSocketClient client = new SampleSocketClient();
-		client.connect("127.0.0.1", 3002);
+		client.connect("127.0.0.1", 3069);
 		try {
 			//if start is private, it's valid here since this main is part of the class
 			client.start();
@@ -80,3 +80,4 @@ public class SampleSocketClient {
 	}
 
 }
+
